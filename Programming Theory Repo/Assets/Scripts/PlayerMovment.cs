ea0ab5b3bb63d92bf12b60583f4b6b2f420f16a3
Nico <niklanus@gmail.com>
2022-01-23 08:39:31 -0300

    Added player controller
    added player movements(left and right)
    added player jump
    added constraint player movement(can´t go outisde the screen)

---
+++ Programming Theory Repo/Assets/Scripts/PlayerMovment.cs
@@ -0,0 +1,69 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovment : MonoBehaviour
+{
+    
+    CharacterController playerController;
+    Vector3 moveVector = Vector3.zero;
+
+    public float moveSpeed;
+    public float jumpSpeed;
+    public float gravity;
+    private float xPos = 8.8f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerMovement();
+        ConstraintsPlayerMovement();
+    }
+    
+    void playerMovement()
+    {
+        moveVector.x = Input.GetAxis("Horizontal") * moveSpeed;
+
+        playerJump();
+        
+        moveVector.y -= gravity * Time.deltaTime;
+        playerController.Move(moveVector * Time.deltaTime);
+
+        
+    }
+    void playerJump()
+    {
+        if (playerController.isGrounded && Input.GetButton("Jump"))
+        {
+            moveVector.y = jumpSpeed;
+        }
+    }
+    
+    void ConstraintsPlayerMovement()
+    {
+        if (playerController.transform.position.x < -xPos)
+        {
+            playerController.transform.position = new Vector3(-xPos, playerController.transform.position.y, 0);
+        }
+        if (playerController.transform.position.x > xPos)
+        {
+            playerController.transform.position = new Vector3(xPos, playerController.transform.position.y, 0);
+        }
+    }
+    //if the player collides with a ladder collider he is allowed to go up
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Ladder"))
+        {
+            moveVector.y = Input.GetAxis("Vertical") * moveSpeed;
+        }
+    }
+
+}
